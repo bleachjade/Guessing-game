7918d7c2a84959838723fa6c4556f7875ab62f51
bleachjade <bleachjade@hotmail.com>
2020-04-10 13:28:42 +0700

    initialize server

---
+++ app/server.js
@@ -0,0 +1,38 @@
+'use strict';
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+const dbName = 'pantip';
+// Create a new MongoClient
+const client = new MongoClient(url, { useUnifiedTopology: true })
+
+// Constants
+const PORT = 8080;
+
+// App
+const app = express();
+
+// Use connect method to connect to the Server
+client.connect(function(err) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  const db = client.db(dbName);
+  const col = db.collection('students');
+
+  app.get('/', (req, res) => {
+    // Get first two documents that match the query
+    col.find({}).limit(1).toArray(function(err, docs) {
+      assert.equal(null, err);
+      res.send(JSON.stringify(docs));
+      client.close();
+    });
+  });
+});
+
+app.listen(PORT);
+console.log(`Running on http://localhost:${PORT}`);
