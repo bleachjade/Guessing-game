530ee50c773f7459b33035cd151b9f8c755b0a44
bleachjade <bleachjade@hotmail.com>
2020-04-10 19:09:32 +0700

    guessing game menu appear on browser

--- app/server.js
+++ app/server.js
@@ -1,5 +1,6 @@
 'use strict';
 const express = require('express');
+const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 // Connection URL
@@ -15,6 +16,26 @@ const PORT = 8080;
 
 // App
 const app = express();
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+const schema = {
+  "stage": 1,
+  "question": ["_", "_", "_", "_"],
+  "guessing": ["*", "*", "*", "*"],
+  "answer": [],
+  "score": {
+    score: 0,
+    duration_secs: 0,
+    duration_text: ""
+  },
+  "fail": 0,
+  "step": 0,
+  "gameStart": null,
+  "gameEnd": null
+}
 
 // Use connect method to connect to the Server
 client.connect(function(err) {
@@ -22,16 +43,23 @@ client.connect(function(err) {
   console.log("Connected successfully to server");
 
   const db = client.db(dbName);
-  const col = db.collection('students');
+  const col = db.collection('game');
 
+  // get index html template
   app.get('/', (req, res) => {
     // Get first two documents that match the query
     col.find({}).limit(1).toArray(function(err, docs) {
       assert.equal(null, err);
-      res.send(JSON.stringify(docs));
+      // res.send(JSON.stringify(docs));
+      res.render('index');
       client.close();
     });
   });
+  //receive data when users hit the button
+  app.post('/', function(req, res) {
+    console.log(req.body.name);
+    res.render('index');
+  })
 });
 
 app.listen(PORT);
